37a9fe8a064a50121d385507444d83084614e51b
bjoerndaigger <bjoern.daigger@gmail.com>
2025-02-12 15:35:19 +0100

    edit form

--- src/app/vocabulary/vocabulary-list/vocabulary-list.component.ts
+++ src/app/vocabulary/vocabulary-list/vocabulary-list.component.ts
@@ -1,13 +1,41 @@
 import { Component, inject } from '@angular/core';
 import { FirebaseService } from '../../services/firebase.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-vocabulary-list',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './vocabulary-list.component.html',
   styleUrl: './vocabulary-list.component.scss'
 })
 export class VocabularyListComponent {
   firebaseService = inject(FirebaseService)
+  isEdited = false;
+  selectedVocabularyIndex: number | null = null;
+  vocabularyId?: string = '';
+  editedVocabulary = {
+    english: '',
+    german: ''
+  };
+
+  editVocabulary(index: number) {
+    this.isEdited = true;
+    this.selectedVocabularyIndex = index;
+    this.vocabularyId = this.firebaseService.vocabularyList[index].id;
+    this.editedVocabulary = {
+      english: this.firebaseService.vocabularyList[index].english,
+      german: this.firebaseService.vocabularyList[index].german
+    };
+  }
+
+  saveEdit() {
+    console.log(this.vocabularyId, this.editedVocabulary);
+  }
+
+  cancelEdit() {
+    this.isEdited = false;
+    this.selectedVocabularyIndex = null;
+    this.vocabularyId = '';
+  }
 }
